a46243fa58bb3b0064a1644b47d2e54bddfe595c
bmeredith <1832575+bmeredith@users.noreply.github.com>
2026-09-05 14:41:20 -0500

    extracted out Ownable, Pausable, SafeMath into their own files

--- contracts/reconstructed/RealmsOfEther.sol
+++ contracts/reconstructed/RealmsOfEther.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.4.18;
 
+import "./SafeMath.sol";
+import "./Ownable.sol";
+import "./Pausable.sol";
+
 import "./FortressStorageProxy.sol";
 import "./BuildingStorageProxy.sol";
 import "./TroupStorageProxy.sol";
@@ -9,81 +13,6 @@ import "./TroupStorageProxy.sol";
 ///
 /// @dev RECONSTRUCTION NOTICE: The original source code for this contract was lost.
 /// This file has been reconstructed in its entirety from the deployed bytecode.
-library SafeMath {
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        assert(c / a == b);
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a / b;
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-}
-
-contract Ownable {
-    address public owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    function Ownable() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-}
-
-contract Pausable is Ownable {
-    event Pause();
-    event Unpause();
-
-    bool public paused = false;
-
-    modifier whenNotPaused() {
-        require(!paused);
-        _;
-    }
-
-    modifier whenPaused() {
-        require(paused);
-        _;
-    }
-
-    function pause() onlyOwner whenNotPaused public {
-        paused = true;
-        Pause();
-    }
-
-    function unpause() onlyOwner whenPaused public {
-        paused = false;
-        Unpause();
-    }
-}
-
 contract RealmsOfEther is Pausable {
     using SafeMath for uint256;
 
