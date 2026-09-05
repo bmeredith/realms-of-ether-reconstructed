eb51d4f0e63d483637b7127324df99827fca220e
bmeredith <1832575+bmeredith@users.noreply.github.com>
2026-09-05 12:21:34 -0500

    -cleanup -removed SafeMath contract

--- contracts/reconstructed/FortressStorage.sol
+++ contracts/reconstructed/FortressStorage.sol
@@ -17,7 +17,6 @@ library SafeMath {
 
         uint256 c = a * b;
         assert(c / a == b);
-        
         return c;
     }
 
@@ -28,14 +27,12 @@ library SafeMath {
 
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
-
         return a - b;
     }
 
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
-
         return c;
     }
 }
